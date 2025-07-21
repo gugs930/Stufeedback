54aa91b8fc22b1953121189cf7a81941f10b9267
gugs930 <guganashri12@gmail.com>
2025-07-21 17:03:43 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/feedbackdb", "root", "Gugana12$$");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
+
